f4d4ee676a00c4f0a180af0b77b359f4496fc940
gherni <hernanegomez@hotmail.com>
2022-10-21 14:28:11 -0300

    serverside props

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const Home: NextPage = (props) => {
+const Home: NextPage<any> = (props) => {
     const {resp} = props
     return (
         <ComparaPage data={resp}/>
